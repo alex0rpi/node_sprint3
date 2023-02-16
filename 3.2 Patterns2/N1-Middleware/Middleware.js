92db07fbc610a1f841bf8cb4ce3764c24996aa31
KarolPxz <karolpelxz@gmail.com>
2023-02-16 10:57:10 +0100

    3.2 middleware understanding

--- 3.2 Patterns2/N1-Middleware/Middleware.js
+++ 3.2 Patterns2/N1-Middleware/Middleware.js
@@ -1,17 +1,14 @@
 /* Middleware
-Crea en un fitxer inicial una petita aplicació que sumi, resti i multipliqui rebent els paràmetres en un JSON.
-Crea en un fitxer extern una classe que emmagatzemi middlewares (funcions).
-Insereix a la invocació middlewares que facin el quadrat, el cub i la divisió entre 2 dels operands inicials en cadascuna de les operacions. Invoca les execucions de la suma, la resta i la multiplicació, de manera que es vagin mostrant per la consola les modificacions que es van fent als valors abans del resultat final. */
+Crea en un fitxer inicial una petita aplicació que sumi, resti i multipliqui rebent els paràmetres en un JSON. Crea en un fitxer extern una classe que emmagatzemi middlewares (funcions). Insereix a la invocació middlewares que facin el quadrat, el cub i la divisió entre 2 dels operands inicials en cadascuna de les operacions. Invoca les execucions de la suma, la resta i la multiplicació, de manera que es vagin mostrant per la consola les modificacions que es van fent als valors abans del resultat final. */
 // Creadora i gestora de middlewares
 
 class Middleware {
-  constructor(operacio) {
-    // el constructor reb una funció
-    this.mdw = operacio; // () => {}
+  constructor(classToBeAdopted) {
+    this.adoptedClass = classToBeAdopted;
+    this.middlewares = []; //empty array for now, will be mapped later
+    this.req = {}; //empty object for now, will be used to get values to work with.
   }
-  use() {}
+  // 
 }
-
-
 
 module.exports = Middleware;
--- 3.2 Patterns2/N1-Middleware/app.js
+++ 3.2 Patterns2/N1-Middleware/app.js
@@ -3,38 +3,30 @@ Crea en un fitxer inicial una petita aplicació que sumi, resti i multipliqui re
 Crea en un fitxer extern una classe que emmagatzemi middlewares (funcions).
 Insereix a la invocació middlewares que facin el quadrat, el cub i la divisió entre 2 dels operands inicials en cadascuna de les operacions. Invoca les execucions de la suma, la resta i la multiplicació, de manera que es vagin mostrant per la consola les modificacions que es van fent als valors abans del resultat final. */
 const Middleware = require('./Middleware');
-const nums = require('./params.json');
 
-const operands = { ...nums };
-let array = Object.values(operands);
 
-const suma = (array) => {
-  const sum = array.reduce((accum, curr) => (accum += curr));
-  console.log(sum);
-};
-
-let mdwSuma = new Middleware(suma)
-
-const resta = (array) => {
-  const resta = array.reduce((accum, curr) => (accum -= curr));
-  console.log(resta);
-};
-
-let mdw3Resta = new Middleware(resta)
-
-const multiplica = (array) => {
-  let multi = array[0];
-  for (let i = 1; i <= array.length - 1; i++) {
-    multi = multi * array[i];
+class Calculadora {
+    suma({ ...operands }) {
+      const array = Object.values(operands);
+      const sum = array.reduce((accum, curr) => (accum += curr));
+      return sum;
+    }
+    resta({ ...operands }) {
+      const array = Object.values(operands);
+      const resta = array.reduce((accum, curr) => (accum -= curr));
+      return resta;
+    }
+    multiplica({ ...operands }) {
+      const array = Object.values(operands);
+      let multi = array[0];
+      for (let i = 1; i <= array.length - 1; i++) {
+        multi = multi * array[i];
+      }
+      return multi;
+    }
   }
-  console.log(multi);
-};
-
-let mdw3multi = new Middleware(multiplica)
-
-mdwSuma.use()
-mdw3Resta.use()
-mdw3multi.use()
+  
+  const calculatrice = new Calculadora();
 
 
 
